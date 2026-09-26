bbb1b1dd0e92c1abade544bdd944ec5821bb37f8
Fooncrad <fooncards@gmail.com>
2026-09-26 15:03:31 +0300

    fix(ui): standardize accessible action sizing

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -21,12 +21,12 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9",
-        "icon-sm": "size-8",
-        "icon-lg": "size-10",
+        default: "h-10 rounded-lg px-4 py-2 has-[>svg]:px-3",
+        sm: "h-9 rounded-lg gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-11 rounded-xl px-6 has-[>svg]:px-4",
+        icon: "size-10 rounded-lg",
+        "icon-sm": "size-9 rounded-lg",
+        "icon-lg": "size-11 rounded-xl",
       },
     },
     defaultVariants: {
